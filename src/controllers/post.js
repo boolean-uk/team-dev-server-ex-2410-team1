51c62aa7fe702c24680a2abe67cb21afc2c88516
ISecka <ISecka@N-NO-01-01-0093>
2024-10-28 16:26:53 +0100

    Get all posts still under development, but working code, added an exapmle of how create post can look

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,7 +1,7 @@
 import { sendDataResponse } from '../utils/responses.js'
 import Post from '../domain/post.js'
 
-export const create = async (req, res) => {
+/*export const create = async (req, res) => {
   const { content } = req.body
 
   if (!content) {
@@ -9,23 +9,48 @@ export const create = async (req, res) => {
   }
 
   return sendDataResponse(res, 201, { post: { id: 1, content } })
-}
+}*/
 
 export const getAll = async (req, res) => {
 
-
-
    try {
-     // Use the Post class to retrieve all posts
      const foundPosts = await Post.findAll()
-
-     // Format the posts for the response
      const formattedPosts = foundPosts.map((post) => post.toJSON())
 
-     return sendDataResponse(res, 200, { posts: formattedPosts })
+     return sendDataResponse(res, 200, { 
+        posts: formattedPosts
+      })
    } catch (error) {
      console.error('Error fetching posts:', error)
      return sendDataResponse(res, 500, { error: 'Failed to fetch posts' })
+   }
+ }
+
+ export const create = async (req, res) => {
+   const { userId, content } = req.body 
+
+   
+   if (!userId || !content) {
+     return sendDataResponse(res, 400, {
+       error: 'User ID and content are required'
+     })
+   }
+
+   try {
+     
+     const newPost = await Post.fromJson({ content, userId }) 
+     const savedPost = await newPost.save() 
+
+     
+     const formattedPost = savedPost.toJSON()
+
+     return sendDataResponse(res, 201, {
+       status: 'success',
+       post: formattedPost
+     })
+   } catch (error) {
+     console.error('Error creating post:', error)
+     return sendDataResponse(res, 500, { error: 'Failed to create post' })
    }
  }
 
